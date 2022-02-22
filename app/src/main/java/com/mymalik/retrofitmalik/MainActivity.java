cdd4de3e3e3c9511c4d1bea6cdbe1a4f15f60f51
aboodemad61 <96075697+aboodemad61@users.noreply.github.com>
2022-02-22 20:06:35 +0200

    TEST

--- app/src/main/java/com/mymalik/retrofitmalik/MainActivity.java
+++ app/src/main/java/com/mymalik/retrofitmalik/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         String newBranch = "newBranch ";
         String malik = "malik AL Asttal";
         String osama = "osama Abood" ;
-        String yahya = " yahya" ;
+        String yahya = " yahya 22" ;
         //
         String base = "https://jsonplaceholder.typicode.com/" ;
         Retrofit retrofit = new Retrofit.Builder().baseUrl(base).addConverterFactory(GsonConverterFactory.create()).build();
