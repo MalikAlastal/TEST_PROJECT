aa7a9030dfe33d8bd8304a5b718a9d958a62a341
aboodemad61 <96075697+aboodemad61@users.noreply.github.com>
2022-02-22 17:24:23 +0200

    ss push

--- app/src/main/java/com/mymalik/retrofitmalik/MainActivity.java
+++ app/src/main/java/com/mymalik/retrofitmalik/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         //
         String newBranch = "newBranch ";
-
+        String s = "ss";
         //
         String base = "https://jsonplaceholder.typicode.com/" ;
         Retrofit retrofit = new Retrofit.Builder().baseUrl(base).addConverterFactory(GsonConverterFactory.create()).build();
