7b0e6a0417e8bc11b55320c302b80db90e380d7f
aboodemad61 <96075697+aboodemad61@users.noreply.github.com>
2022-02-22 19:08:28 +0200

    malik txt

--- app/src/main/java/com/mymalik/retrofitmalik/MainActivity.java
+++ app/src/main/java/com/mymalik/retrofitmalik/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         //
         String test = "abood";
         String newBranch = "newBranch ";
-        String malik = "malik";
+        String malik = "malik AL Asttal";
         //
         String base = "https://jsonplaceholder.typicode.com/" ;
         Retrofit retrofit = new Retrofit.Builder().baseUrl(base).addConverterFactory(GsonConverterFactory.create()).build();
